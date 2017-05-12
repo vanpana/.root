b20af1b59b6dd726abdc2cc8d0936753e2dc83b6
Dorinel Panaite <van.panaite@gmail.com>
2017-05-12 12:31:45 -0700

    Created form and implemented loading from file

---
+++ root/archiveuploader.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace root
+{
+    partial class archiveuploader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.projectsummary = new System.Windows.Forms.TextBox();
+            this.projectsummaryLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.dueDateLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // projectsummary
+            // 
+            this.projectsummary.Location = new System.Drawing.Point(12, 22);
+            this.projectsummary.Multiline = true;
+            this.projectsummary.Name = "projectsummary";
+            this.projectsummary.Size = new System.Drawing.Size(361, 315);
+            this.projectsummary.TabIndex = 0;
+            // 
+            // projectsummaryLabel
+            // 
+            this.projectsummaryLabel.AutoSize = true;
+            this.projectsummaryLabel.Location = new System.Drawing.Point(12, 6);
+            this.projectsummaryLabel.Name = "projectsummaryLabel";
+            this.projectsummaryLabel.Size = new System.Drawing.Size(86, 13);
+            this.projectsummaryLabel.TabIndex = 1;
+            this.projectsummaryLabel.Text = "Project Summary";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 351);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(124, 13);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "Status: Not uploaded yet";
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(298, 351);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 3;
+            this.uploadButton.Text = "Upload!";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            // 
+            // dueDateLabel
+            // 
+            this.dueDateLabel.AutoSize = true;
+            this.dueDateLabel.Location = new System.Drawing.Point(12, 372);
+            this.dueDateLabel.Name = "dueDateLabel";
+            this.dueDateLabel.Size = new System.Drawing.Size(54, 13);
+            this.dueDateLabel.TabIndex = 4;
+            this.dueDateLabel.Text = "Due: date";
+            // 
+            // archiveuploader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 394);
+            this.Controls.Add(this.dueDateLabel);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.projectsummaryLabel);
+            this.Controls.Add(this.projectsummary);
+            this.Name = "archiveuploader";
+            this.Text = "project_name";
+            this.Load += new System.EventHandler(this.archiveuploader_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox projectsummary;
+        private System.Windows.Forms.Label projectsummaryLabel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Label dueDateLabel;
+    }
+}
---
+++ root/archiveuploader.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace root
+{
+    public partial class archiveuploader : Form
+    {
+        public string[] parseFile(string path)
+        {
+            string[] parsed = new string[3];
+            parsed[0] = ""; parsed[1] = ""; parsed[2] = "";
+            int counter = 0;
+            try
+            {
+                using (StreamReader reader = File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = reader.ReadLine()) != null)
+                    {
+                        parsed[counter++] = s;
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+
+            if (parsed[0] == "" || parsed[1] == "" || parsed[2] == "")
+                throw new Exception("Invalid file.");
+
+            if (parsed[2].Length != 10)
+                throw new Exception("Invalid read date.");
+
+            return parsed;
+        }
+
+        public DateTime parseDate(string date)
+        {
+            string[] ints = date.Split('.');
+            DateTime parsedDate = new DateTime(Int32.Parse(ints[2]), Int32.Parse(ints[1]), Int32.Parse(ints[0]), 0, 0 ,0);
+            return parsedDate;
+        }
+
+        public archiveuploader()
+        {
+            InitializeComponent();
+        }
+
+        private void archiveuploader_Load(object sender, EventArgs e)
+        {
+            string[] parsed = new string[3];
+            try
+            {
+                parsed = parseFile("data/project1.txt");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(String.Format("Exception encountered: {0}", exc));
+                this.Close();
+            }
+            
+            this.Text = parsed[0];
+            this.projectsummary.Text = parsed[1];
+            this.dueDateLabel.Text = "Due: " + parsed[2];
+
+            if (File.Exists("data/project1.txt"))
+                this.statusLabel.Text = "Status: Uploaded";
+
+            DateTime today = DateTime.Now;
+            DateTime due = parseDate(parsed[2]);
+
+            int dateCompareResult = DateTime.Compare(today, due); 
+
+            if (dateCompareResult > 0)
+                this.uploadButton.Enabled = false;
+        }
+    }
+}
