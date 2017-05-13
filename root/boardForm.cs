aee27f32a828ba84612d68351d4b1cb74a80cafe
Dorinel Panaite <van.panaite@gmail.com>
2017-05-13 16:44:28 +0300

    Merged all framework forms

---
+++ root/boardForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace root
+{
+    public partial class boardForm : Form
+    {
+        string[,] help = new string[20,4];
+        public static string username;
+
+        public static string[] filePaths;
+        public static string[] fileTitles;
+
+
+        string[,] parsefile(string path)
+        {
+            string[,] parsed = new string[20, 4];
+
+            for (int j = 0; j < 20; j++)
+                parsed[j, 0] = "";
+
+            int counter = 0;
+            try
+            {   // Open the text file using a stream reader.
+                StreamReader file = new StreamReader(path);
+                string[] split;
+                while ((split = file.ReadLine().Split(',')) != null)
+                {
+                    for (int i = 0; i < 4; i++)
+                        parsed[counter, i] = split[i];
+                    counter++;
+                }
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return parsed;
+        }
+
+        string helpText(string usrn, string helptype, string name)
+        {
+            if (helptype == "1")
+                return name + ", lecture, posted by " + usrn;
+            return name + ", project, posted by " + usrn;
+        }
+
+        public boardForm(string username)
+        {
+            InitializeComponent();
+            boardForm.username = username;
+
+            help = parsefile("data/help/helpboard.txt");
+
+            for (int i = 0; i < 10; i++)
+                if (help[i, 0] != username && help[i,0] != "")
+                    helpBox.Items.Add(helpText(help[i,0], help[i,1], help[i,2]));
+        }
+
+        private void boardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            string curItem = helpBox.SelectedItem.ToString();
+
+            // Find the string in ListBox2.
+            int index = helpBox.FindString(curItem);
+
+            hintWindow hw = new hintWindow(help[index, 0], help[index, 1], help[index, 2], help[index, 3]);
+            hw.Show();
+        }
+
+        private void yourHelpsButton_Click(object sender, EventArgs e)
+        {
+            filePaths = Directory.GetFiles("data/users/" + username + "/help/");
+            if (filePaths.Length == 0)
+                MessageBox.Show("No help yet!");
+            else
+            {
+                helpFeedback hf = new helpFeedback();
+                fileTitles = new string[filePaths.Length];
+
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    string[] temp = filePaths[i].Split('/');
+                    boardForm.fileTitles[i] = temp[temp.Length - 1];
+                    MessageBox.Show(fileTitles[i].ToString());
+                    hf.helpBox.Items.Add(fileTitles[i]);
+                }
+
+                hf.Show();
+            }
+        }
+    }
+}
---
+++ root/helpFeedback.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace root
+{
+    partial class helpFeedback
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helpBox = new System.Windows.Forms.ListBox();
+            this.helpPreview = new System.Windows.Forms.TextBox();
+            this.likeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // helpBox
+            // 
+            this.helpBox.FormattingEnabled = true;
+            this.helpBox.Location = new System.Drawing.Point(0, 12);
+            this.helpBox.Name = "helpBox";
+            this.helpBox.Size = new System.Drawing.Size(379, 95);
+            this.helpBox.TabIndex = 2;
+            this.helpBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // helpPreview
+            // 
+            this.helpPreview.Location = new System.Drawing.Point(0, 113);
+            this.helpPreview.Multiline = true;
+            this.helpPreview.Name = "helpPreview";
+            this.helpPreview.Size = new System.Drawing.Size(379, 119);
+            this.helpPreview.TabIndex = 4;
+            // 
+            // likeButton
+            // 
+            this.likeButton.Location = new System.Drawing.Point(290, 238);
+            this.likeButton.Name = "likeButton";
+            this.likeButton.Size = new System.Drawing.Size(75, 23);
+            this.likeButton.TabIndex = 5;
+            this.likeButton.Text = "button1";
+            this.likeButton.UseVisualStyleBackColor = true;
+            // 
+            // helpFeedback
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 264);
+            this.Controls.Add(this.likeButton);
+            this.Controls.Add(this.helpPreview);
+            this.Controls.Add(this.helpBox);
+            this.Name = "helpFeedback";
+            this.Text = "helpFeedback";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.ListBox helpBox;
+        private System.Windows.Forms.TextBox helpPreview;
+        private System.Windows.Forms.Button likeButton;
+    }
+}
